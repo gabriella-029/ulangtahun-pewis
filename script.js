6a0e1524b25f6a6ad97801b42108fa4fba69b2af
Gabriella Joice S <76762826+gabriella-029@users.noreply.github.com>
2025-06-19 09:14:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -128,8 +128,13 @@ const quotes = [
   "Terima kasih sudah menjadi dirimu 💛",
   "Kalau capek, istirahat ya. Pelan-pelan aja 🤍",
   "Kamu pantas bahagia, Priss 🌷",
-  "Aku bersyukur pernah dan masih ada kamu",
+  "Aku bersyukur pernah dan masih ada dirimu, Pris",
   "Selalu ada peluk meski dari jauh 🤗"
+  "Jangan bosan jadi manusia 🤗"
+  "Selamat bertumbuh!"
+  "If Flower need time to bloom, so you are"
+  "Bahagia mulainya dari diri sendiri"
+  "Kita yang mengusahakan ekspektasi kita"
 ];
 
 let qIndex = 0;
